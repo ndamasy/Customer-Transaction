689820e34839d5c77ef1d3e3071f29c1db10b197
ndamasy <nermein4686@gmail.com>
2024-07-18 19:25:27 +0300

    ndamasy/add-customerData-chart

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,7 @@
 
 // //fetch json file
 var customerData = [];
+let mrChart;
 fetch('data.json')
    .then(function (response) {
       return response.json();
@@ -9,7 +10,7 @@ fetch('data.json')
    .then(function (result) {
       const finalDataToUse = dataPreparation(result);
       displayCustomerData(finalDataToUse);
-      creatChart(finalDataToUse, 'bar');
+      creatChart(finalDataToUse, 'bar')
    })
    .catch(function (error) {
       console.error('Error fetching data:', error);
@@ -17,26 +18,26 @@ fetch('data.json')
 
 function dataPreparation(obj) {
 
-    if (obj && obj.customers.length > 0 && obj.transactions.length > 0) {
-        for (let i = 0; i < obj.customers.length; i++) {
-            const customerTransactions = obj.transactions.filter((transaction) => {
-                return transaction.customer_id === obj.customers[i].id;
-            });
-            customerTransactionsAmount = customerTransactions.length > 1 ? customerTransactions.reduce((a, b) => {
-                return a.amount + b.amount;
-            }) : customerTransactions[0].amount;
+   if (obj && obj.customers.length > 0 && obj.transactions.length > 0) {
+      for (let i = 0; i < obj.customers.length; i++) {
+         const customerTransactions = obj.transactions.filter((transaction) => {
+            return transaction.customer_id === obj.customers[i].id;
+         });
+         customerTransactionsAmount = customerTransactions.length > 1 ? customerTransactions.reduce((a, b) => {
+            return a.amount + b.amount;
+         }) : customerTransactions[0].amount;
 
-            customerData.push({
-                customer_id: obj.customers[i].id,
-                name: obj.customers[i].name,
-                amount: customerTransactionsAmount,
-                transaction_id: obj.transactions[i].id,
-                date: obj.transactions[i].date
-            });
-        }
-    }
+         customerData.push({
+            customer_id: obj.customers[i].id,
+            name: obj.customers[i].name,
+            amount: customerTransactionsAmount,
+            transaction_id: obj.transactions[i].id,
+            date: obj.transactions[i].date
+         });
+      }
+   }
 
-    return customerData;
+   return customerData;
 }
 
 // display data in table
@@ -44,9 +45,11 @@ let customerTransactions;
 
 const table = document.querySelector('.table-body-data')
 function displayCustomerData(customerData) {
-    var customerTableData = '';
-    for (let i = 0; i < customerData.length; i++) {
-        customerTableData += `
+     var customerTableData = '';
+    if (customerData &&  customerData.customers.length > 0 &&  customerData.transactions.length > 0) {
+         for (let i = 0; i < customerData.customers.length; i++) {
+               customerTableData += `
+            
               <tr>
                 <th scope="row">${customerData[i].customer_id}</th>
                 <td>${customerData[i].name}</td>
@@ -77,3 +80,4 @@ searchInput.addEventListener("input", function (e) {
 });
 
 
+}
